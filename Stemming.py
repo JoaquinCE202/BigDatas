ba36727f2489d22864bd14157c997ab7aa06293a
Joaquin Casusol <81270957+JoaquinCE202@users.noreply.github.com>
2024-03-21 11:23:25 -0500

    Add files via upload

---
+++ Stemming.py
@@ -0,0 +1,34 @@
+import nltk
+from nltk.stem import PorterStemmer
+from collections import Counter
+
+# Descargar los datos necesarios de NLTK, solo es necesario hacerlo una vez
+# nltk.download('punkt')
+
+# Inicializamos el stemmer
+stemmer = PorterStemmer()
+
+def leer_y_contar(ruta_archivo):
+    frecuencias = Counter()
+    
+    try:
+        with open(ruta_archivo, 'r', encoding='utf-8') as archivo:
+            # Leer el archivo línea por línea
+            for linea in archivo:
+                palabra = linea.strip().lower()  # Eliminamos espacios y convertimos a minúsculas
+                # Aplicamos el proceso de stemming a cada palabra
+                raiz_palabra = stemmer.stem(palabra)
+                # Contabilizamos la raíz de la palabra
+                frecuencias[raiz_palabra] += 1
+    except Exception as e:
+        print(f"Se produjo un error al leer el archivo: {e}")
+        return
+    
+    # Imprimir las frecuencias de las palabras
+    for palabra, frecuencia in frecuencias.items():
+        print(f"Palabra: {palabra}, Frecuencia: {frecuencia}")
+
+# Ruta del archivo a leer
+ruta_archivo = r"C:\Users\Leoncio Casusol\Downloads\archivo20GB.txt"
+
+leer_y_contar(ruta_archivo)
